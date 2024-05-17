b8057d0223830d297a52618013fb66199cd37432
arshiadadras <arshiadadras1234@gmail.com>
2024-05-17 17:42:35 +0330

    fix: Main fixed to get inputs.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import org.example.shipping.Package;
+import org.example.shipping.state.DeliveredState;
+import org.example.shipping.strategy.ExpressStrategy;
+import org.example.shipping.strategy.StandardStrategy;
+
 import java.util.Scanner;
 
 public class Main {
@@ -12,57 +17,51 @@ public class Main {
         double packageWeight = scanner.nextDouble();
 
         // Initialize package and delivery state
-//        Package package = new Package(packageWeight);
-//        DeliveryState deliveryState = DeliveryState.IN_TRANSIT;
-//
-//        while (deliveryState != DeliveryState.DELIVERED) {
-//            // Display current package status and shipping options
-//            System.out.println("\nPackage Status: " + deliveryState);
-//            System.out.println("1. Standard Shipping (2.5 $/kg)");
-//            System.out.println("2. Express Shipping (3.5 $/kg)");
-//
-//            // Get shipping method choice
-//            System.out.print("Enter shipping method (1/2): ");
-//            int shippingMethodChoice = scanner.nextInt();
-//
-//            // Calculate shipping cost based on selected method
-//            double shippingCost;
-//            switch (shippingMethodChoice) {
-//                case 1:
-//                    shippingCost = packageWeight * STANDARD_SHIPPING_RATE;
-//                    break;
-//                case 2:
-//                    shippingCost = packageWeight * EXPRESS_SHIPPING_RATE;
-//                    break;
-//                default:
-//                    System.out.println("Invalid shipping method choice. Please enter 1 or 2.");
-//                    continue;
-//            }
-//
-//            // Update package shipping method and cost
-//            package.setShippingMethod(shippingMethodChoice);
-//            package.setShippingCost(shippingCost);
-//
-//            // Display shipping cost
-//            System.out.printf("Shipping cost: $%.2f\n", shippingCost);
-//
-//            // Get delivery status update
-//            System.out.print("Enter delivery status update (1/2): ");
-//            int deliveryStatusUpdate = scanner.nextInt();
-//
-//            // Update delivery state based on status update
-//            switch (deliveryStatusUpdate) {
-//                case 1:
-//                    deliveryState = DeliveryState.DELIVERED;
-//                    System.out.println("Package delivered!");
-//                    break;
-//                case 2:
-//                    break;
-//                default:
-//                    System.out.println("Invalid delivery status update. Please enter 1 or 2.");
-//            }
-//        }
-//
-//        scanner.close();
+        Package myPackage = new Package(packageWeight);
+
+        while (!(myPackage.getState() instanceof DeliveredState)) {
+            // Display current package status and shipping options
+            myPackage.printStatus();
+            System.out.println("1. Standard Shipping (2.5 $/kg)");
+            System.out.println("2. Express Shipping (3.5 $/kg)");
+
+            // Get shipping method choice
+            System.out.print("Enter shipping method (1/2): ");
+            int shippingMethodChoice = scanner.nextInt();
+
+            switch (shippingMethodChoice) {
+                case 1:
+                    myPackage.setStrategy(new StandardStrategy());
+                    break;
+                case 2:
+                    myPackage.setStrategy(new ExpressStrategy());
+                    break;
+                default:
+                    System.out.println("Invalid delivery status update. Please enter 1 or 2.");
+            }
+
+            // Calculate shipping cost based on selected method
+
+            System.out.println("1. Package is Delivered!");
+            System.out.println("2. Keep Package in transit!");
+
+            // Get delivery status update
+            System.out.print("Enter delivery status update (1/2): ");
+            int deliveryStatusUpdate = scanner.nextInt();
+
+            // Update delivery state based on status update
+            switch (deliveryStatusUpdate) {
+                case 1:
+                    myPackage.updateState();
+                    break;
+                case 2:
+                    break;
+                default:
+                    System.out.println("Invalid delivery status update. Please enter 1 or 2.");
+            }
+        }
+
+        System.out.println("Final Price After The Package is Delivered: " + myPackage.calculateShippingPrice() + "$");
+        scanner.close();
     }
 }
